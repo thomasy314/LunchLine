add1af49b0c4f73e10183946eb29400c6a97bb51
Thomas <thomasy314@gmail.com>
2016-04-27 09:49:37 -0700

    do stuff

---
+++ app/src/main/java/lunchline/favflav/com/lunchline/MainActivity.java
@@ -0,0 +1,91 @@
+package lunchline.favflav.com.lunchline;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TabHost;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    EditText nameTxt, moneyTxt;
+    Button enterBtn;
+    TabHost tabHost;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameTxt = (EditText) findViewById(R.id.name);
+        moneyTxt =  (EditText) findViewById(R.id.money);
+        tabHost = (TabHost) findViewById(R.id.tabHost);
+
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec("Criteria");
+        tabSpec.setContent(R.id.tabCriteria);
+        tabSpec.setIndicator("Criteria");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("Food");
+        tabSpec.setContent(R.id.tabFood);
+        tabSpec.setIndicator("Food");
+        tabHost.addTab(tabSpec);
+
+        enterBtn = (Button) findViewById(R.id.enter);
+        enterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "it works!!!", Toast.LENGTH_SHORT).show();
+                //tabHost.sele
+            }
+        });
+
+
+        nameTxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                enterBtn.setEnabled(!nameTxt.getText().toString().trim().isEmpty());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
